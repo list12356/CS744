bf69cf2776d585041c34c3c1a3c87493ade845e8
list12356 <list12356@sina.com>
2019-09-20 19:46:16 +0000

    customize url

--- assignment1/pagerank.py
+++ assignment1/pagerank.py
@@ -7,27 +7,30 @@ def contribute(rank, edges):
     for x in edges:
         yield (x, rank/num_nbr)
 
-def main(input_path, output_path, debug=0, iter_num=10):
-    conf = SparkConf().setAppName("PageRank").setMaster("spark://3.19.211.19:7077")
+def main(input_path, output_path, debug=0, iter_num=10,\
+            spark_ip='172.31.12.172', directory=False):
+    conf = SparkConf().setAppName("PageRank").setMaster("spark://{!s}:7077".format(spark_ip))
     sc = SparkContext(conf=conf)
     distFile = sc.textFile(input_path)
-    splitted = distFile.map(lambda s: s.decode('utf-8').split())
+    splitted = distFile.map(lambda s: [x.rstrip() for x in s.split('\t')])
+    splitted = splitted.filter(lambda s: len(s) == 2)
     out_edges = splitted.map(lambda s: (s[0], s[1])).groupByKey()
     graph = out_edges.mapValues(list)
     rank = out_edges.mapValues(lambda x: 1.0)
 
     # rank.persist()
+    graph.persist()
     for i in range(iter_num):
         temp = rank.join(graph)
         temp = temp.flatMap(lambda x: contribute(x[1][0], x[1][1])).reduceByKey(add)
         rank = temp.mapValues(lambda x: x*0.85 + 0.15)
         
     
-    # rank.saveAsTextFile(output_path)
+    rank.saveAsTextFile(output_path)
 
-    with open("/home/shitao/debug_pagerank.out", 'w+') as debug_file:
-        for key, x in rank.collectAsMap().items():
-            debug_file.write("{!s} : {!s}\n".format(key, x))
+    # with open("/home/shitao/debug_pagerank.out", 'w+') as debug_file:
+    #    for key, x in rank.collectAsMap().items():
+    #        debug_file.write("{!s} : {!s}\n".format(key, x))
 
 
 if __name__=="__main__":
@@ -35,5 +38,6 @@ if __name__=="__main__":
     parser.add_argument('input_path')
     parser.add_argument('output_path')
     parser.add_argument('--debug', default=0)
+    parser.add_argument('spark_ip')
     args = parser.parse_args()
-    main(args.input_path, args.output_path, debug=args.debug)
+    main(args.input_path, args.output_path, debug=args.debug, spark_ip=args.spark_ip)
--- assignment1/run.sh
+++ assignment1/run.sh
@@ -1,3 +1,7 @@
-~/spark-2.4.4-bin-hadoop2.7/bin/spark-submit  --master local ~/CS744/assignment1/pagerank.py hdfs://172.31.45.228:9000/CS744/assignment1/web-BerkStan.txt hdfs://172.31.45.228:9000/CS744/assignment1/rank-BerkStan.txt 
+HDFS_IP=172.31.12.172
 
-# ~/spark-2.4.4-bin-hadoop2.7/bin/spark-submit  --master local ~/CS744/assignment1/pagerank.py hdfs://172.31.45.228:9000/CS744/assignment1/test.txt hdfs://172.31.45.228:9000//CS744/assignment1/test-out.txt  --debug=1
+hdfs dfs -rm -r /CS744/assignment1/rank-BerkStan.txt
+~/spark-2.4.4-bin-hadoop2.7/bin/spark-submit  --master local ~/CS744/assignment1/pagerank.py hdfs://$HDFS_IP:9000/CS744/assignment1/web-BerkStan.txt hdfs://$HDFS_IP:9000/CS744/assignment1/rank-BerkStan.txt  $HDFS_IP
+
+
+# ~/spark-2.4.4-bin-hadoop2.7/bin/spark-submit  --master local ~/CS744/assignment1/pagerank.py hdfs://$HDFS_IP:9000/enwiki-pages-articles/*.xml-* hdfs://$HDFS_IP:9000/enwiki-pages-articles.rank
